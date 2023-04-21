64879029cae355bbe83967874ce230d7545c3cf6
EuanMur <125983116+EuanMur@users.noreply.github.com>
2023-04-21 21:15:47 +0000

    Pending changes exported from your codespace

--- FilesInOut.java
+++ FilesInOut.java
@@ -4,6 +4,8 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import java.lang.Number;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * 
@@ -13,22 +15,55 @@ import java.lang.Number;
 public class FilesInOut {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
+   
+        File inputFile = new File("input.txt");
+        File outputFile = new File("output.txt");       
 
-        // Set up a new Scanner to read the input file.
-        // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
-        // Then add code to modify the text to the output format.
+        try {
+            // Set up a new Scanner to read the input file.
+            Scanner scanner = new Scanner(inputFile);
+            
+            while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
 
-        // Set up a new PrintWriter to write the output file.
-        // Add suitable code into the above processing (because you need to do this line by line also.
-        // That is, read a line, write a line, loop.
+             // Parse name and date from input line
+             String[] parts = line.split("(?=\\d)(?<!\\d)");
+             String name = parts[0];
+             String dateStr = parts[1];
 
-        // Finally, add code to read the filenames as arguments from the command line.
+             String[] bothNames = name.split(" ");
+             
+             bothNames[0] = bothNames[0].substring(0, 1).toUpperCase() + bothNames[0].substring(1);
+             bothNames[1] = bothNames[1].substring(0, 1).toUpperCase() + bothNames[1].substring(1);
+             String fullName = bothNames[0] + " " + bothNames[1];
 
-        System.out.println("You need to add your own code to do anything");
+             SimpleDateFormat format = new SimpleDateFormat("ddMMyyyy");
+             SimpleDateFormat newDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+             try {
 
+                Date date = format.parse(dateStr);
+                String newDate = newDateFormat.format(date);
+                System.out.println(fullName + " " + newDate);
+                
+                try{
+
+                    FileWriter writer = new FileWriter("output.txt");
+                    PrintWriter printWriter = new PrintWriter(writer);
+                    printWriter.println(fullName + " " + newDate);
+                    printWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
     } // main
 
 } // FilesInOut
---
+++ FormatNamesm.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import java.lang.Number;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * 
+ * CSCU9T4 Java strings and files exercise.
+ *
+ */
+public class FormatNamesm {
+
+    public static void main(String[] args) {
+
+        File inputFile = new File("inputm.txt");
+        File outputFile = new File("output.txt");
+
+
+        try {
+            // Set up a new Scanner to read the input file.
+            Scanner scanner = new Scanner(inputFile);
+            
+            while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+             // Parse name and date from input line
+             String[] parts = line.split("(?=\\d)(?<!\\d)");
+             String name = parts[0];
+             String dateStr = parts[1];
+
+             String[] bothNames = name.split(" ");
+             
+             bothNames[0] = bothNames[0].toUpperCase();
+             bothNames[1] = bothNames[1].toUpperCase();
+             bothNames[2] = bothNames[2].toUpperCase();
+             String fullName = bothNames[0] + " " + bothNames[1] + " " + bothNames[2];
+        
+             SimpleDateFormat format = new SimpleDateFormat("ddMMyyyy");
+             SimpleDateFormat newDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+             try {
+
+                Date date = format.parse(dateStr);
+                String newDate = newDateFormat.format(date);
+                System.out.println(fullName + " " + newDate);
+                
+                try{
+
+                    FileWriter writer = new FileWriter("output.txt");
+                    PrintWriter printWriter = new PrintWriter(writer);
+                    printWriter.println(fullName + " " + newDate);
+                    printWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            
+        }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+    } // main
+
+} // FilesInOut
